d6965a640a36e780d46016ce5f03a47bc021b0fe
writemorecode <gustav.ae.karlsson@gmail.com>
2024-01-12 23:01:44 +0100

    added check that number of train/test images match number of labels

--- main.cpp
+++ main.cpp
@@ -11,21 +11,31 @@ int main() {
     return 1;
   }
 
-  auto test_data{read_image_data(FILE_TEST_IMAGE)};
-  if (test_data.size() == 0) {
-    std::cerr << "Error: Failed to read testing image data.\n";
-    return 1;
-  }
-
   auto train_label{read_label_data(FILE_TRAIN_LABEL)};
   if (train_label.size() == 0) {
     std::cerr << "Error: Failed to read training labels.\n";
     return 1;
   }
 
+  if (train_data.size() != train_label.size()) {
+    std::cerr << "Number of training images does not match number of training labels.\n";
+    return 1;
+  }
+
+  auto test_data{read_image_data(FILE_TEST_IMAGE)};
+  if (test_data.size() == 0) {
+    std::cerr << "Error: Failed to read testing image data.\n";
+    return 1;
+  }
+
   auto test_label{read_label_data(FILE_TEST_LABEL)};
   if (train_label.size() == 0) {
     std::cerr << "Error: Failed to read testing labels.\n";
+    return 1;
+  }
+
+  if (test_data.size() != test_label.size()) {
+    std::cerr << "Number of testing images does not match number of testing labels.\n";
     return 1;
   }
 
